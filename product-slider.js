782a50b867001dc6efa8a5fc0d7fd3db7cff250c
Bilal Ahmed <bilalahmedim@gmail.com>
2024-07-04 17:19:02 +0530

    product image slider in progress.

--- product-slider.js
+++ product-slider.js
@@ -16,15 +16,26 @@ carusel.forEach(e => (e.addEventListener('click', retrieveImage)));
 
 // Slide Left
 const slideLeft = e => {
-  console.log(e.target);
-  currentSlide();
+  // console.log(e.target);
+  changeSlide('neg');
 }
 
 
-// Find Current SLider
+// Find the Current SLider
 const currentSlide = e => {
   const currentSlide_number = zoomable__img.getAttribute('data-current-slide');
-  console.log(currentSlide_number);
+  return currentSlide_number;
+}
+// end find the current slider
+
+const changeSlide = e => {
+  const slideNum = Number(currentSlide());
+  const slideLoop = [1, 2, 3];
+  console.log(slideNum)
+
+  if (e === 'neg') {
+    if (slideNum === 1) { }
+  }
 }
 
 
