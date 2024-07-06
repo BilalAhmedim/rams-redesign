62b5cca35ef2434852074673fd66c880cd5f0342
Bilal Ahmed <bilalahmedim@gmail.com>
2024-07-07 02:11:24 +0530

    product slider complete.

--- product-slider.js
+++ product-slider.js
@@ -7,9 +7,10 @@ const carusel = document.querySelectorAll('.carusel'),
 
 // Image carusel
 const retrieveImage = event => {
-  const Element = event.target.getAttribute('data-src-large');
-  console.log(Element);
-  zoomable__img.src = Element;
+  const largeSrc = event.target.getAttribute('data-src-large'),
+    imgSrc = event.target.getAttribute('data-img-src');
+  zoomable__img.setAttribute('data-current-slide', imgSrc)
+  zoomable__img.src = largeSrc;
 }
 carusel.forEach(e => (e.addEventListener('click', retrieveImage)));
 
@@ -34,26 +35,34 @@ const currentSlide = e => {
 
 // Change Slide Right / Left
 const changeSlide = e => {
-  const slideNum = Number(currentSlide());
+  const slideNum = Number(currentSlide()),
+    slide_data1 = document.querySelector('.carusel[data-img-src="1"]').getAttribute('data-src-large'),
+    slide_data2 = document.querySelector('.carusel[data-img-src="2"]').getAttribute('data-src-large'),
+    slide_data3 = document.querySelector('.carusel[data-img-src="3"]').getAttribute('data-src-large');
   // debugger
   if (e === 'neg') {
     if (slideNum === 1) {
       zoomable__img.setAttribute('data-current-slide', '3')
+      zoomable__img.src = slide_data3;
     } else if (slideNum === 2) {
       zoomable__img.setAttribute('data-current-slide', '1')
+      zoomable__img.src = slide_data1;
     } else {
       zoomable__img.setAttribute('data-current-slide', '2')
+      zoomable__img.src = slide_data2;
     }
   } else if (e === 'pos') {
     if (slideNum === 1) {
       zoomable__img.setAttribute('data-current-slide', '2')
+      zoomable__img.src = slide_data2;
     } else if (slideNum === 2) {
       zoomable__img.setAttribute('data-current-slide', '3')
+      zoomable__img.src = slide_data3;
     } else {
       zoomable__img.setAttribute('data-current-slide', '1')
+      zoomable__img.src = slide_data1;
     }
   }
-  console.log(slideNum)
 }
 
 // end Change Slide Right / Left
