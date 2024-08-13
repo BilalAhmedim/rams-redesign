bbfa73d56d82280f3eeb9a0266bfdd544b87ef92
Bilal Ahmed <bilalahmedim@gmail.com>
2024-08-13 15:11:02 +0530

    gallery section

--- src/index.php
+++ src/index.php
@@ -42,9 +42,7 @@ if (is_front_page()){ // Homne Page Hero Section
       'description'=>get_field('approach_dec')
     ],
   ];
-    
   ?>
-
   <section class="section is-medium is-primary has-background">
     <div class="container">
       <h1 class="title is-1 pb-5">Services</h1>
@@ -64,7 +62,79 @@ if (is_front_page()){ // Homne Page Hero Section
       </div>
 
     </div>
-  </section>
+  </section><?// service section?>
+
+  <?php
+  // Gallery Section
+  $gallery_bath = get_field('baths');
+  $gallery_fs= get_field('farmhouse-sinks');
+  $gallery_us = get_field('undermount-sinks');
+  $gallery_as = get_field('additional-sinks');
+  $gallery_hoods = get_field('hoods');
+  ?>
+  <section class="section is-primary is-medium">
+
+<div class="container">
+  <h1 class="title is-1 pb-5">Products Gallery</h1>
+
+  <div class="columns is-variable is-8 pb-4">
+
+    <div class="column is-one-third">
+      <a href="#" class="">
+        <figure class="image">
+          <img class="is-rounded" src="<?php echo $gallery_bath['sizes']['medium_large'] .'"' . ' alt='.$gallery_bath['alt'];?>" />
+        </figure>
+        <h2 class="subtitle is-4 pt-2 has-text-centered"><?php echo $gallery_bath['title']?></h2>
+      </a>
+    </div>
+
+    <div class="column is-one-third">
+      <a href="#" class="">
+        <figure class="image">
+          <img class="is-rounded" src="<?php echo $gallery_fs['sizes']['medium_large'] .'"' . ' alt='.$gallery_fs['alt'];?>" />
+        </figure>
+        <h2 class="subtitle is-4 pt-2 has-text-centered"><?php echo $gallery_fs['title']?></h2>
+      </a>
+    </div>
+
+    <div class="column is-one-third">
+      <a href="#" class="">
+        <figure class="image">
+          <img class="is-rounded" src="<?php echo $gallery_us['sizes']['medium_large'] .'"' . ' alt='.$gallery_us['alt'];?>" />
+        </figure>
+        <h2 class="subtitle is-4 pt-2 has-text-centered"><?php echo $gallery_us['title']?></h2>
+      </a>
+    </div>
+
+  </div>
+
+  <div class="columns is-justify-content-center">
+
+    <div class="column is-one-third">
+      <a href="#" class="">
+        <figure class="image">
+          <img class="is-rounded" src="<?php echo $gallery_as['sizes']['medium_large'] .'"' . ' alt='.$gallery_as['alt'];?>" />
+        </figure>
+        <h2 class="subtitle is-4 pt-2 has-text-centered"><?php echo $gallery_as['title']?></h2>
+      </a>
+    </div>
+
+    <div class="column is-one-third">
+      <a href="#" class="">
+        <figure class="image">
+          <img class="is-rounded" src="<?php echo $gallery_hoods['sizes']['medium_large'] .'"' . ' alt='.$gallery_hoods['alt'];?>" />
+        </figure>
+        <h2 class="subtitle is-4 pt-2 has-text-centered"><?php echo $gallery_hoods['title']?></h2>
+      </a>
+    </div>
+
+  </div>
+
+</div>
+
+</section>
+<?php //gallery section?>
+
 <?php
 }
 ?>
